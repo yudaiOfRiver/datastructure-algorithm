a63b3bffdedf824bc5e567767ec1a84ec729b917
yudaiOfRiver <ykawa412@gmail.com>
2023-03-22 10:29:20 +0900

    update binary_search

--- binary_search.py
+++ binary_search.py
@@ -4,8 +4,17 @@ def binary_search(nums: list[int], target: int) -> int:
         m = (l+r) // 2
         if target == nums[m]:
             return m
-        elif target < nums[m]:
+        elif nums[m] > target:
             r = m-1
         elif nums[m] < target:
             l = m+1
-    return l
+#    return None # 値がなければ None を返す
+    return l     # 値がなければ 挿入箇所 を返す
+
+
+if __name__ == '__main__':
+    nums = [1, 3, 4, 6, 7, 8]
+    print(binary_search(nums, 1))
+    print(binary_search(nums, 6))
+    print(binary_search(nums, 8))
+    print(binary_search(nums, 5))
